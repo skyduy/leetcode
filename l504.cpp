3626ad4207151fca94ce0d1e27738cc1e4cc5ded
YuJun <skyduy@outlook.com>
2020-02-13 13:57:06 +0800

    add leetcode 504

--- l504.cpp
+++ l504.cpp
@@ -1,7 +1,32 @@
 // TODO https://leetcode.com/problems/base-7/
 #include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+	string convertToBase7(int num) {
+		if (num == 0) return "0";
+		string ans;
+		bool neg = false;
+		if (num < 0) {
+			neg = true;
+			num = -num;
+		}
+		while (num > 0) {
+			ans.push_back(num % 7 + '0');
+			num /= 7;
+		}
+		if (neg) {
+			ans.push_back('-');
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
+	}
+};
 
 int main() {
-	std::cout << "Hello world, leetcode 504" << std::endl;
+	Solution s;
+	cout << s.convertToBase7(100) << ' ' << s.convertToBase7(-7) << endl;
 	return 0;
 }
---
+++ tmp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+#include<bitset>
+
+using namespace std;
+
+int main_tmp() {
+	uint32_t a = 5;
+	bitset<32> b(a);
+	cout << b << endl;
+	return 0;
+}
